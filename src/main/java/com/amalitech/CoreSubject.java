2dfb825c0525a23f1078be76881f0b6241989282
LennoxOwusuAfriyie <lennox.afriyie@amalitech>
2025-12-15 11:44:26 +0000

    created core subject with its objects and methods for testing

---
+++ src/main/java/com/amalitech/CoreSubject.java
@@ -0,0 +1,17 @@
+package com.amalitech;
+
+//Represents a mandatory core subject that all students must take.
+public class CoreSubject extends Subject {
+
+    public CoreSubject(String subjectName, String subjectCode) {
+        super(subjectName, subjectCode);
+    }
+
+    @Override
+    public String getSubjectType() {
+
+        return "Core";
+    }
+
+}
+
